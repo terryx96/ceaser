29ae46ab35c0b063404b09a6cb8151cb72850c09
Terry <terry.wade@valpo.edu>
2019-04-11 22:40:00 -0500

    Shift the characters of the message by a random num

--- ceaser.py
+++ ceaser.py
@@ -1,4 +1,15 @@
+import random
+
 file = open('./plaintext.txt', 'r')
-print(file.read())
+
+plaintext = file.read()
+ciphertext = ""
+shift = random.randint(1, 26)
+
+for char in plaintext:
+    newChar = chr( ord(char) + shift )
+    ciphertext = ciphertext + newChar
+
+print(ciphertext)
 
 file.close()
