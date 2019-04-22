9cfec5a9ed0a1d78247af77815505a1f5fae2a4e
Terry <terry.wade@valpo.edu>
2019-04-21 19:42:33 -0500

    Move code into a method

--- ceaser.py
+++ ceaser.py
@@ -3,27 +3,30 @@ import random
 file = open('./plaintext.txt', 'r', encoding = "utf-8")
 
 plaintext = file.read().lower().replace(" ", "")
-ciphertext = ""
-shift = random.randint(1, 26)
-index = 0
 
-for char in plaintext:
-    newchar = ""
-    if((ord(char) + shift) > 122):
-        newChar = chr( ord(char) + shift - 26)
-    else:
-        newChar = chr( ord(char) + shift ) 
-    ciphertext = ciphertext + newChar
-    
-    if(index == 4):
-        index = 0
-        ciphertext = ciphertext + " "
-    else:
-        index = index + 1
+def ceaser(plain):
+    shift = random.randint(1, 26)
+    ciphertext = ""
+    index = 0
+    for char in plaintext:
+        newchar = ""
+        if((ord(char) + shift) > 122):
+            newChar = chr( ord(char) + shift - 26)
+        else:
+            newChar = chr( ord(char) + shift ) 
+        ciphertext = ciphertext + newChar
+        
+        if(index == 4):
+            index = 0
+            ciphertext = ciphertext + " "
+        else:
+            index = index + 1
+    return ciphertext
 
+cipher = ceaser(plaintext)
 
 
 output = open('./ciphertext.txt', 'w')
-output.write(ciphertext)
+output.write(cipher)
 
 file.close()
