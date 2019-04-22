d695b59d1b34c9240e4e1055c7ae74c9e3277255
Terry <terry.wade@valpo.edu>
2019-04-21 19:40:09 -0500

    Encrypt and split in groups of five

--- ceaser.py
+++ ceaser.py
@@ -1,15 +1,29 @@
 import random
 
-file = open('./plaintext.txt', 'r')
+file = open('./plaintext.txt', 'r', encoding = "utf-8")
 
-plaintext = file.read()
+plaintext = file.read().lower().replace(" ", "")
 ciphertext = ""
 shift = random.randint(1, 26)
+index = 0
 
 for char in plaintext:
-    newChar = chr( ord(char) + shift )
+    newchar = ""
+    if((ord(char) + shift) > 122):
+        newChar = chr( ord(char) + shift - 26)
+    else:
+        newChar = chr( ord(char) + shift ) 
     ciphertext = ciphertext + newChar
+    
+    if(index == 4):
+        index = 0
+        ciphertext = ciphertext + " "
+    else:
+        index = index + 1
 
-print(ciphertext)
+
+
+output = open('./ciphertext.txt', 'w')
+output.write(ciphertext)
 
 file.close()
